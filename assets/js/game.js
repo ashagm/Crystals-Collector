6c74872a7e44881408e5c68fe7723942dd52b742
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-12-13 20:17:27 -0500

    adding sounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,38 +1,51 @@
 
 $(document).ready(function(){
 
+const sounds = 
+{
+	'select': "assets/sounds/click.mp3",
+	'win': "assets/sounds/win.wav",
+	'start': "assets/sounds/start.mp3",
+	'lose': "assets/sounds/lose.wav"
+}
+
 	let wins = 0, losses = 0;
 	let $targetScore;
 	let $totalScore;
 	let crystalsArray = [];
 
 	$('button').on('click', function(){
+		playSound('select');
 		$totalScore += parseInt($(this).val());
 		$("#total-score").text($totalScore);
 
+		displayResults();
+	});
+
+	function displayResults(){
 		if($totalScore > $targetScore){
 			console.log("you lost!");
+			playSound('lose');
 			$('#losses').text(++losses);
-			initGame();
+
+			setTimeout(initGame, 2000);
+			
 		}else if($totalScore === $targetScore){
 			console.log("you win!");
+			playSound('win');
 			$('#wins').text(++wins);
-			initGame();
+
+			setTimeout(initGame, 2000);
 		}
-	});
+	}
 	
 	function getRandomNumber(lowLimit, highLimit){
 		return Math.floor(Math.random() * (highLimit - lowLimit + 1) + lowLimit); //stackoverflow
 	}
 
-	function initGame(){
-		console.log("Game begins....");
-		crystalsArray = [];	
-		$totalScore = 0;
-		$targetScore = getRandomNumber(19,120);
+	function initCrystalValues(){
 
-		$('#target-score').text($targetScore);
-		$("#total-score").text(0);
+		crystalsArray = [];	
 
 		for(let index = 0; index < 4; index++){
 			crystalsArray.push(getRandomNumber(1, 12));	
@@ -41,7 +54,29 @@ $(document).ready(function(){
 		}	
 	}
 
+	function initGame(){
+		console.log("Game begins....");
+		playSound('start');
+
+		$totalScore = 0;
+		$targetScore = getRandomNumber(19,120);
+
+		$('#target-score').text($targetScore);
+		$("#total-score").text(0);
+
+		initCrystalValues();
+	}
+
 	initGame();
+
+
+function playSound(sound){
+	console.log(sound);
+	let audio = document.createElement("audio");
+	audio.src = sounds[sound];
+	audio.play();
+}
+
 });
 
 
